368355911f857509a417c80b644435b3241a40e9
Gabe <Carrollgbusiness@gmail.com>
2026-06-17 16:28:39 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/car_dealership_api/Model/DealershipModel.java
@@ -0,0 +1,50 @@
+package com.pluralsight.car_dealership_api.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "dealerships")
+public class DealershipModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int dealershipId;
+
+    private String name;
+
+    private String address;
+
+    private String phone;
+
+    public int getDealershipId() {
+        return dealershipId;
+    }
+
+    public void setDealershipId(int dealershipId) {
+        this.dealershipId = dealershipId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/main/java/com/pluralsight/car_dealership_api/Model/VehicleModel.java
@@ -0,0 +1,79 @@
+package com.pluralsight.car_dealership_api.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "vehicles")
+public class VehicleModel {
+    @Id
+    private String vin;
+
+    private int year;
+
+    private String make;
+
+    private String vehicleType;
+
+    private String color;
+
+    private int odometer;
+
+    private double price;
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
